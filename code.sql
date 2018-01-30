24e5f0fca9152fe5a6ea56810d5d31e799b1aec1
mgmilton <matt.milton@gmail.com>
2018-01-30 14:03:04 -0700

    updates through ex15

--- code.sql
+++ code.sql
@@ -6,7 +6,8 @@ CREATE TABLE person (
     id INTEGER PRIMARY KEY,
     first_name TEXT,
     last_name TEXT,
-    age INTEGER
+    age INTEGER,
+    dob DATETIME
 );
 
 CREATE TABLE pet (
@@ -19,31 +20,43 @@ CREATE TABLE pet (
 
 CREATE TABLE person_pet (
     person_id INTEGER,
-    pet_id INTEGER
+    pet_id INTEGER,
+    purchased_on DATETIME
 );
-INSERT INTO person (id, first_name, last_name, age) VALUES (0, 'Zed', 'Shaw', 37);
-INSERT INTO pet (id, name, breed, age, dead) VALUES (0, 'Fluffy', 'Unicorn', 1000, 0);
-INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1);
-INSERT INTO person VALUES (1, 'Matt', 'Milton', 27);
-INSERT INTO person VALUES (2, 'Kurt', 'Godel', 33);
-INSERT INTO pet VALUES (2, 'Roscoe', 'Australian Shepperd', 4, 0);
-INSERT INTO pet VALUES (3, 'Bertrand', 'Philosopher', 97, 0);
+
+INSERT INTO person (id, first_name, last_name, age) 
+    VALUES (0, "Zed", "Shaw", 37);
+
+INSERT INTO pet (id, name, breed, age, dead)
+    VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
 INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
-INSERT INTO person_pet VALUES (0, 1);
-INSERT INTO person_pet VALUES (1, 2);
-INSERT INTO person_pet VALUES (2, 3);
+INSERT INTO person_pet VALUES (0, 1, date('now'));
+SELECT * FROM person;
+
+SELECT name, age FROM pet;
+
+SELECT name, age FROM pet WHERE dead = 0;
+
+SELECT * FROM person WHERE first_name != "Zed";
+SELECT pet.id, pet.name, pet.age, pet.dead
+    FROM pet, person_pet, person
+    WHERE
+    pet.id = person_pet.pet_id AND
+    person_pet.person_id = person.id AND
+    person.first_name = "Zed";
 /* make sure there's dead pets */
 SELECT name, age FROM pet WHERE dead = 1;
 
-/* delete the dead robot pet */
+/* aww poor robot */
 DELETE FROM pet WHERE dead = 1;
 
-/*make sure the robot is gone */
+/* make sure the robot is gone */
 SELECT * FROM pet;
 
-/* let's ressurect the robot */
-INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 0);
-
+/* let's resurrect the robot */
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 0);
 
 /* the robot LIVES! */
 SELECT * FROM pet;
---
+++ code_ex1_ex14/code.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS pet;
+DROP TABLE IF EXISTS person_pet;
+
+CREATE TABLE person (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE pet (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT,
+    age INTEGER,
+    dead INTEGER
+);
+
+CREATE TABLE person_pet (
+    person_id INTEGER,
+    pet_id INTEGER
+);
+INSERT INTO person (id, first_name, last_name, age) VALUES (0, 'Zed', 'Shaw', 37);
+INSERT INTO pet (id, name, breed, age, dead) VALUES (0, 'Fluffy', 'Unicorn', 1000, 0);
+INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1);
+INSERT INTO person VALUES (1, 'Matt', 'Milton', 27);
+INSERT INTO person VALUES (2, 'Kurt', 'Godel', 33);
+INSERT INTO pet VALUES (2, 'Roscoe', 'Australian Shepperd', 4, 0);
+INSERT INTO pet VALUES (3, 'Bertrand', 'Philosopher', 97, 0);
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
+INSERT INTO person_pet VALUES (0, 1);
+INSERT INTO person_pet VALUES (1, 2);
+INSERT INTO person_pet VALUES (2, 3);
+/* make sure there's dead pets */
+SELECT name, age FROM pet WHERE dead = 1;
+
+/* delete the dead robot pet */
+DELETE FROM pet WHERE dead = 1;
+
+/*make sure the robot is gone */
+SELECT * FROM pet;
+
+/* let's ressurect the robot */
+INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 0);
+
+
+/* the robot LIVES! */
+SELECT * FROM pet;
