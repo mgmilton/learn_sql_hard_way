6e9c5199a646395a69db1960230306a12ffe3ad0
mgmilton <matt.milton@gmail.com>
2018-01-29 18:23:17 -0700

    updates through ex3

---
+++ ex3.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (id, first_name, last_name, age) VALUES (0, 'Zed', 'Shaw', 37);
+INSERT INTO pet (id, name, breed, age, dead) VALUES (0, 'Fluffy', 'Unicorn', 1000, 0);
+INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1);
+INSERT INTO person VALUES (1, 'Matt', 'Milton', 27);
+INSERT INTO person VALUES (2, 'Kurt', 'Godel', 33);
+INSERT INTO pet VALUES (2, 'Roscoe', 'Australian Shepperd', 4, 0);
+INSERT INTO pet VALUES (3, 'Bertrand', 'Philosopher', 97, 1);
